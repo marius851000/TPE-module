788d6f63a62246a84d9073fe4bd5e17661f6b8d0
marius851000 <mariusdavid@laposte.net>
2019-01-20 18:35:42 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function(sails) {
   var send = require("./lib/send.js");
   var setup = require("./lib/configure.js");
-  var exec = require("./lib/exec.js")
+  var exec = require("./lib/exec.js");
   return {
     send: send,
     setup: setup,
